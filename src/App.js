3910e30fce01a2a11134506aa17dbf358154e277
Justin Zheng <83163126+justinzzy-code@users.noreply.github.com>
2024-10-16 23:20:47 -0400

    m

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Project Testing123
+          React Project Testing!!!
         </p>
         <a
           className="App-link"
